c77e9f1c77f4bc2e176136f2eb0a55eae59ef2fe
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-12-26 05:33:23 +0000

    change App.js

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
 export default function App() {
+  const [Slr, setSlr] = useState('');
+  const [usd, setUsd] = useState('');
+
+  const convertToUsd = () => {
+    const conversionRate = 0.003;
+    const converted = (parseFloat(Slr) * conversionRate).toFixed(2);
+    setUsd(converted);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text style={styles.title}>LKR to USD Converter</Text>
+       <TextInput style={styles.input}
+     mode="outlined"
+     placeholder = "Enter amount in LKR"
+     value={Slr}
+    onChangeText={setSlr}
+     />
+
+      <Button title="Convert" onPress={convertToUsd} />
+
+      {usd ? <Text style={styles.result}>USD: ${usd}</Text> : null}
     </View>
   );
 }
@@ -17,4 +34,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+  },
+  result: {
+    fontSize: 20,
+    color: '#333',
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
 });
